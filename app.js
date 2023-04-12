7596480172aae0da42ed04073ea4fbebed852dd0
Naby Saar <nabysarr16@gmail.com>
2023-04-12 11:51:55 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const date = new Date();
+
+  const today = date.toLocaleDateString("en-US", options);
+
+  res.render("list", { theDate: today, newItem: items });
+});
+
+app.post("/", function (req, res) {
+  let newElement = req.body.newListItem;
+
+  items.push(newElement);
+
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Hey! we're up!");
+});
